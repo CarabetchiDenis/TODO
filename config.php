bc6a31f96a1b6731366e6250e258cdd9f5cc375d
CarabetchiDenis <90355622+CarabetchiDenis@users.noreply.github.com>
2021-09-21 16:28:54 -0400

    Update config.php

--- config.php
+++ config.php
@@ -7,8 +7,8 @@ $database = "todo_app";
 
 $conn = mysqli_connect($server, $username, $password, $database);
 
-if (!$conn) {
-    die("<script>alert('Connection Failed.')</script>");
+if (!$conn === false) {
+    die("ERROR Colud not connect.". mysqli_connect_error());
 }
 
 ?>
