30c99bd8346092cb5971a80eb2da56f8500add36
CarabetchiDenis <90355622+CarabetchiDenis@users.noreply.github.com>
2021-09-21 22:05:45 -0400

    Add files via upload
    modified

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css "href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>TODO List</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
    
 </head>
 <body>
-    <form action="" method="post" align="center">
+    <form action="" method="post" >
         <h1>Welcome TO DO</h1>
         <div class="wrapper">
             <div class="inputFields">
@@ -19,23 +20,34 @@
             </div>
             <hr>
             <div class="content"  >
-             <ul>
-                 <li>
-                    <span class="text">new record.</span>
-                 </li>
-                 <li>
-                    <span class="text">create.</span>
-                 </li>
-                 <li>
-                    <span class="text">read.</span>
-                 </li>
-                 <li>
-                    <span class="text">blabla.</span>
-                 </li>
-             </ul>
+                <ul>
+                    <li>
+                        <span class="text">new record.</span>
+                        <i class="fa fa-trash" aria-hidden="true"></i>
+
+                    </li>
+                    <li>
+                        <span class="text">create.</span>
+                        <i class="fa fa-trash" aria-hidden="true"></i>
+                    </li>
+                    <li>
+                        <span class="text">read.</span>
+                        <i class="fa fa-trash" aria-hidden="true"></i>
+                    </li>
+                    <li>
+                        <span class="text">blabla.</span>
+                        <i class="fa fa-trash" aria-hidden="true"></i>
+                    </li>                    
+                </ul>
+                <div class="pending-text">Total 4 Écriture</div>
             </div>
         </div>
+        
     </form>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function)
+    </script>
 
 
 
@@ -44,7 +56,7 @@
 
 
 
-	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	
 	<script>
 	
         // Add Task
